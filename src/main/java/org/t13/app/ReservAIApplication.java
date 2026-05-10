d42139738a876b9537e88a0d3f0822be144d1b46
thanish13 <thanishmalai13@gmail.com>
2026-05-10 12:52:30 +0530

    Init commit for front end

---
+++ src/main/java/org/t13/app/ReservAIApplication.java
@@ -0,0 +1,14 @@
+package org.t13.app;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservAIApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservAIApplication.class, args);
+    }
+
+}
